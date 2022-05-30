2fab4b2ac7ea3ccf37d93868c3a9c0dc886d4618
KantWang <dngp93@gmail.com>
2022-05-30 19:31:08 +0900

    'C:/Users/Wang/AppData/Local/Microsoft/WindowsApps/python3.10.exe

---
+++ 0530/kantwang.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+def solution(s):
+    nums = list(map(int, s.split()))
+    nums.sort()
+    ans = [nums[0], nums[-1]]
+    
+    # print(' '.join(list(map(str, ans))))
+    return ' '.join(list(map(str, ans)))
+
+solution("1 2 3 5")
+solution("-3 -2 1 5")
