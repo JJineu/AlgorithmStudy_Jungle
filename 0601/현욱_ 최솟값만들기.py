ba85f26d963aa797f1961cb22fce3aee9e745ba3
jocy0412 <jakob1@naver.com>
2022-06-01 19:42:26 +0900

    min A 수정

--- 0601/현욱_ 최솟값만들기.py
+++ 0601/현욱_ 최솟값만들기.py
@@ -1,15 +1,15 @@
 # 테스트케이스 통과 - 효율성 통과 X
-def solution(A,B):
-    result = 0
-    for i in range(len(A)) :
-        minA = min(A)
-        maxB = max(B)
-        A.remove(maxB)
-        B.remove(maxB)
-        result += i * maxB
-    answer = result
+# def solution(A,B):
+#     result = 0
+#     for i in range(len(A)) :
+#         minA = min(A)
+#         maxB = max(B)
+#         A.remove(maxB)
+#         B.remove(maxB)
+#         result += minA * maxB
+#     answer = result
 
-    return answer
+#     return answer
 
 # 정답
 def solution(A,B):
