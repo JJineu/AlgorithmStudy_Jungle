879349646867ccf7f997b3e653696c7d8b30f004
kimhyunjin <joice.kim7@gmail.com>
2022-06-01 19:33:29 +0900

    hj

---
+++ 0601/현진_최솟값만들기.py
@@ -0,0 +1,19 @@
+# 시간초과
+def solution(A,B):
+    answer = 0
+
+    A.sort
+    B.sort(reverse = True)
+    for i in range(len(A)):
+        answer += A[i]*B[i]
+            
+    return answer
+
+# 정답코드 
+def solution(A,B):
+    answer = 0
+    A = sorted(A)
+    B = sorted(B, reverse=True)
+
+    answer = sum([a * b for a, b in zip(A, B)])
+    return answer
