dfc5acce1ba55eec0b1545d15b17114dadb8f83d
jocy0412 <jakob1@naver.com>
2022-06-02 17:14:32 +0900

    현욱 튜플

---
+++ 0602/현욱_튜플.py
@@ -0,0 +1,14 @@
+def solution(s):
+    answer = []
+    data = []
+    for s in s[2:-2].split('},{') :
+        data.append(s.split(','))
+        # print('정렬전',data)
+        data.sort(key=len)
+        # print('정렬후',data)
+    for i in data :
+        for j in i :
+            if int(j) not in answer:
+                answer.append(int(j))
+                break
+    return answer
