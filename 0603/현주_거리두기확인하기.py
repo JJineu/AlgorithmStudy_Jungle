713bcd4abe1c844d258c648129f036427f5921ee
jessy1001 <jessyhjoo@gmail.com>
2022-06-05 19:45:48 +0900

    현주 0605

--- 0603/현주_거리두기확인하기.py
+++ 0603/현주_거리두기확인하기.py
@@ -1,40 +1,39 @@
 import sys
 input = sys.stdin.readline
+sys.setrecursionlimit(100000)
 
 #상, 하, 좌, 우
-nx = [-1, 1, 0, 0]
-ny = [0, 0, -1, 1]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+visited = [[0 for _ in range(5)] for _ in range(5)]
+# print(visited)
+
+def check(room, x, y, depth=0):
+    visited[x][y] = 1
+    if depth == 2:
+        return True
+
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < 5 and 0 <= ny < 5:
+            if visited[nx][ny]: continue
+            if room[nx][ny] == 'P':
+                return False
+            elif room[nx][ny] == 'O':
+                check(room, nx, ny, depth+1)
+            # else: continue #벽이면 다른 방향 탐색
+    return True
 
 def solution(slist):
     answer = []
     for room in slist: #["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"] 즉 각 교실
-        seats = []
-        for row in room:
-            seats.append(list(row)) #"POOOP" -> ['P', 'O', 'O', 'O', 'P']
-        
-        def check(x, y, depth=0):
-            if depth ==  2:
-                return True
-
-            for i in range(4):
-                dx = x + nx[i]
-                dy = y + ny[i]
-                if 0 <= dx < 5 and 0 <= dy < 5:
-                    if room[dx][dy] == 'P':
-                        return False
-                    elif room[dx][dy] == 'O':
-                        depth += 1
-                        check(dx, dy, depth)
-                    else: return True #벽이면
-            # return True
-        
         Flag = False
         for x in range(5):
             for y in range(5):
                 if room[x][y] == 'P':
-                    if check(x, y, 0):
-                        continue
-                    else:
+                    if not check(room, x, y, 0):
+                        print(check(room, x, y, 0))
                         answer.append(0)
                         Flag = True
                         break
@@ -42,14 +41,15 @@ def solution(slist):
                 break
         else: 
             answer.append(1)
+        print(answer)
 
-    print(answer)
+    # print(answer)
     return answer
     
 
 
 
 solution([["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"], ["POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"], ["PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"], ["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"], ["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"]])
-solution([["OPXPO", "OOOOO", "OOOOO", "OOOOO", "OOOOO"], ["XXXXX", "XXXXX", "XXXXX", "XXXXX", "XXXXX"], ["PPPPP", "PPPPP", "PPPPP", "PPPPP", "PPPPP"], ["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"], ["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"]])
+# solution([["OPXPO", "OOOOO", "OOOOO", "OOOOO", "OOOOO"], ["XXXXX", "XXXXX", "XXXXX", "XXXXX", "XXXXX"], ["PPPPP", "PPPPP", "PPPPP", "PPPPP", "PPPPP"], ["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"], ["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"]])
 # solution(["OPXPO", "OOOOO", "OOOOO", "OOOOO", "OOOOO"])
 # ["OOOOO", "OOOOO", "OOOOO", "OOOOO", "OOOOO"]
---
+++ 0605/현주_문자 숫자열과 영단어.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+numbers = {0: "zero", 1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six", 7: "seven", 8: "eight", 9: "nine"}
+
+
+def solution(slist):
+    keylist = list(map(str, numbers.keys()))
+    letters = []
+    answer = []
+
+    for i in slist:
+        if i in keylist:
+            answer.append(int(i))
+        else: # if string
+            if len(letters) > 0:
+                letters.append(i)
+                n_string = ''.join(letters)
+                # print(n_string)
+                if n_string in numbers.values():
+                    # print([k for k, v in numbers.items() if v == n_string])
+                    answer.append(*[k for k, v in numbers.items() if v == n_string])
+                    letters.clear()
+            else:
+                letters.append(i)
+                # print(letters)
+    return int(''.join(list(map(str,answer))))
+    # print(*answer, sep = '')
+    
+
+solution("one4")
+solution("2three45sixseven")
