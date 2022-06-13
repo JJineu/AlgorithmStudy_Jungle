ccae7e1a79933abeb6ed088e767bb2a458741f53
kimhyunjin <joice.kim7@gmail.com>
2022-06-13 20:09:24 +0900

    re

--- 0613/현진_줄서는방법.py
+++ 0613/현진_줄서는방법.py
@@ -1,26 +1,42 @@
-#시간초과
-from itertools import permutations
+# #시간초과
+# from itertools import permutations
 
-def solution(n, k):
-    answer = []
-    people = list(range(1,n+1))
-    s = []
-    # s = permutations(people,n)
-    cnt = 0
-    for i in permutations(people,n):
-        cnt += 1
-        if cnt == k:
-            for j in range(n):
-                answer.append(i[j])
-            # answer = i
-    # answer = s[k]
-    print(answer)
-    return answer
-solution(3,5)
+# def solution(n, k):
+#     answer = []
+#     people = list(range(1,n+1))
+#     s = []
+#     # s = permutations(people,n)
+#     cnt = 0
+#     for i in permutations(people,n):
+#         cnt += 1
+#         if cnt == k:
+#             for j in range(n):
+#                 answer.append(i[j])
+#             # answer = i
+#     # answer = s[k]
+#     print(answer)
+#     return answer
+# solution(3,5)
 
 
 # 첫번째 수 정해지면 경우의수-> (n-1)!
 # 두번째 수 정해지면 경우의수-> (n-2)!
 # (n-1)!이 n 개 나열되어 있는 형태
-# k번째는 k%n == 0 -> k//n 그룹
-# k%n != 0 -> k//n +1 그룹 
+# k번째는 k%n == 0 -> k//n -1 그룹
+# k%n != 0 -> k//n 그룹 
+
+import math
+
+def solution(n, k):
+    answer = []
+    numbers = list(range(1, n+1))
+    
+    for i in range(n, 0, -1):
+        div, k = divmod(k, math.factorial(i-1))
+        if k == 0:
+            answer.append(numbers.pop(div-1))
+        else :
+             answer.append(numbers.pop(div))
+
+    return answer
+solution(3,5)
