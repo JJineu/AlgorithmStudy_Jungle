365f032d80204e820902f0d62baaed501ae8430c
jessy1001 <jessyhjoo@gmail.com>
2022-06-16 19:25:21 +0900

    현주 0616

---
+++ 0616/현주_네트워크.py
@@ -0,0 +1,24 @@
+import sys
+sys.setrecursionlimit(1000000)
+
+def solution(n, computers):
+    count = 0
+    visited = [0 for _ in range(n)]
+
+    def dfs(i):
+        visited[i] = 1
+        for j in range(len(computers)):
+            if j != i and computers[i][j] == 1:
+                if not visited[j]:
+                    dfs(j)
+
+    for i in range(n):
+        if not visited[i]:
+            dfs(i)
+            count += 1
+    # print(count)
+    return count
+    
+
+
+solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]])
