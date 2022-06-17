14948490f943e5c69622b05c487cc6f868aba16b
haein-side <75101582+haein-side@users.noreply.github.com>
2022-06-17 19:55:58 +0900

    Update 해인_정수삼각형.py

--- 0617/해인_정수삼각형.py
+++ 0617/해인_정수삼각형.py
@@ -12,9 +12,8 @@ def solution(t):
     else :
         dp[0][1] = t[0][1]
         
-        
         for i in range(1, len(t)):
-            for j in range(1, i+2):
+            for j in range(1, i+2): # 처음과 끝에 0을 둔 곳은 따로 dp 값 갱신하지 않고 처음 0인 곳과 마지막 0인 곳은 제외하고 dp값 계산해줌
                 dp[i][j] = max(dp[i-1][j-1] + t[i][j], dp[i-1][j] + t[i][j])
         
         return max(dp[-1])
