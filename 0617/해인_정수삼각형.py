a58325b3b3806012b39b24cbbc25bb1458f2d17a
haein-side <75101582+haein-side@users.noreply.github.com>
2022-06-17 19:54:39 +0900

    Create 해인_정수삼각형.py

---
+++ 0617/해인_정수삼각형.py
@@ -0,0 +1,20 @@
+def solution(t):
+    for i in range(len(t)):
+        t[i] = [0] + t[i] + [0]
+        # t〉[[0, 7, 0], [0, 3, 8, 0], [0, 8, 1, 0, 0], [0, 2, 7, 4, 4, 0], [0, 4, 5, 2, 6, 5, 0]]
+    dp = [[] for _ in range(len(t))]
+    for i in range(len(t)):
+        dp[i] = [0 for _ in range(i+3)]
+        # dp [[0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]]
+    
+    if len(t) == 1:
+        return t[0][1]
+    else :
+        dp[0][1] = t[0][1]
+        
+        
+        for i in range(1, len(t)):
+            for j in range(1, i+2):
+                dp[i][j] = max(dp[i-1][j-1] + t[i][j], dp[i-1][j] + t[i][j])
+        
+        return max(dp[-1])
