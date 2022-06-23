6c105ebd80dbc9ddf45e05d4478923120386707a
kimhyunjin <joice.kim7@gmail.com>
2022-06-23 20:10:21 +0900

    hj

---
+++ 0623/현진/현진_1269_대칭차집합.py
@@ -0,0 +1,31 @@
+from sys import stdin 
+input = stdin.readline
+
+def main():
+    a, b = map(int, input().split())
+    li_a = list(map(int, input().split()))
+    ans_a = len(li_a)
+    li_b = list(map(int, input().split()))
+    ans_b = len(li_b)
+    for i in li_b:
+        li_a.append(i)
+    ans_c = len(set(li_a))
+    ans = ans_a + ans_b - ans_c #교집합
+    print(ans_c - ans)
+    
+main()
+
+from sys import stdin 
+input = stdin.readline
+
+def main():
+    n, m = map(int, input().split())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+    for i in b:
+        a.append(i)
+    set(a)
+    a.sort()
+    # print(*a)
+    print(' '.join(list(map(str, a))))
+main()
