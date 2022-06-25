b76985c6e01ca488364e47ebf9d88c37bc17de10
kimhyunjin <joice.kim7@gmail.com>
2022-06-25 20:18:56 +0900

    hj

---
+++ 0625/해인/현진0625/1062_가르침.py
@@ -0,0 +1,11 @@
+from sys import stdin
+input = stdin.readline
+
+def main():
+    n, k = map(int, input().split())
+    for i in range(n):
+        s = input()
+    a = ['b','d','e','f','g','h','j','k','l','m','o','p','q','r','s','u','v','w','x','y','z']
+    b = ['a', 'n', 't', 'i', 'c']
+     
+main()
