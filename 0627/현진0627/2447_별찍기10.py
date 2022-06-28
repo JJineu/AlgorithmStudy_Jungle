504e24c235cd3544f0529689e69f440ced85d747
kimhyunjin <joice.kim7@gmail.com>
2022-06-28 19:08:08 +0900

    mk

---
+++ 0627/현진0627/2447_별찍기10.py
@@ -0,0 +1,17 @@
+from sys import stdin 
+input = stdin.readline
+
+# 분할정복, 재귀인데...
+
+def main():
+    def star1():
+        print('* * *')
+    def star2():
+        print('*')
+    def empty():
+        print(' ')
+
+    def cur(depth):
+
+    cur()
+main()
