733c0d45d927831d3dc69aba1cf4b8cc80bbad5f
haein-side <75101582+haein-side@users.noreply.github.com>
2022-06-28 21:34:27 +0900

    Update 해인_k번째수3.py

--- 0627/해인/해인_k번째수3.py
+++ 0627/해인/해인_k번째수3.py
@@ -10,7 +10,7 @@ while start <= end:
     mid = (start + end) // 2     # i*j 값이 mid일 때
     tmp = 0
     for i in range(1, n+1):
-        tmp += min(n, mid // i)  # 각 행을 돌면서 mid 이항인 수의 개수를 셈
+        tmp += min(n, mid // i)  # 각 행을 돌면서 mid 이하인 수의 개수를 셈
     
     if tmp <= k:
         # print('tmp:',tmp)
